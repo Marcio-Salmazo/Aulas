2c389219ae6df6ff9069ceb77f7c4a8516d9ff3a
Marcio Salmazo <marcio.salmazo19@gmail.com>
2026-08-05 17:29:12 -0300

    Aula 2 de LP

--- UNIUBE-LP/Praticas/Operadores.c
+++ UNIUBE-LP/Praticas/Operadores.c
@@ -10,6 +10,13 @@ int main()
      //Acentos do português
      setlocale(LC_ALL, "Portuguese"); 
      
+     printf("valor inicial das variáveis:");
+     printf("x = %d\n",x);
+     printf("y = %d\n",y);
+     printf("z = %d\n",z);
+     
+     printf("\n");
+     
     //soma
     soma = x+y+z;
     printf("Soma 1 - x+y+z: %d\n", soma);
@@ -32,17 +39,64 @@ int main()
     
     //multiplicação
     mul = x*y;
-    printf("Multiplicação 1 - y-z: %d\n", mul);
+    printf("Multiplicação 1 - x*y: %d\n", mul);
     mul = y*(x-z);
-    printf("Multiplicação 2 - x-y: %d\n", mul);
+    printf("Multiplicação 2 - y*(x-z): %d\n", mul);
 
     printf("\n");
     
     //divisão (inteira)
     div = 7/z;
     printf("Multiplicação 1 - y-z: %d\n", div);
-    div = 2/5;
+    div = z/5;
     printf("Multiplicação 2 - x-y: %d\n", div);
+    
+    printf("\n");
+    
+    //modulo da divisão (resto)
+    //funciona apenas com valores inteiros 
+    //para imprimir o símbolo de porcentagem é necessário escrever %%
+    int mod;
+    mod = 70%33;
+    printf("Modulo (Resto) 1 - 7%%z: %d\n", mod);
+    mod = y%x;
+    printf("Modulo (Resto) 2 - y%%x: %d\n", mod);
+    
+    printf("\n");
+    
+    //incremento e decremento
+    int i = 1;
+    printf("variável i = %d\n",i);
+    i++;
+    printf("incremento = %d\n",i);
+    i--;
+    printf("decremento = %d\n",i);
+    
+    printf("\n");
+    
+    //Operador lógico AND
+    //Esse operador funciona apenas com 0 e 1 (Falso e Verdadeiro)
+    int and;
+    and = 0 && 1;
+    printf("0 AND 1 = %d\n",and);
+    and = 1 && 1;
+    printf("1 AND 1 = %d\n",and);
+    
+    //Operador lógico OR
+    //Esse operador funciona apenas com 0 e 1 (Falso e Verdadeiro)
+    int or;
+    or = 0 || 1;
+    printf("0 OR 1 = %d\n",or);
+    or = 0 || 0;
+    printf("0 OR 0 = %d\n",or);
+    
+    //Operador lógico NOT
+    //Esse operador funciona apenas com 0 e 1 (Falso e Verdadeiro)
+    int not;
+    not = !0;
+    printf("NOT 0 = %d\n",not);
+    not = !1;
+    printf("NOT 1 = %d\n",not);
     
     getch();
     return 0;
